462069c9831117afa78a803d8760b18aa3fae92b
Falk20 <petruk08@mail.ru>
2019-03-26 15:34:53 +0300

    create project

---
+++ js/interactive.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+
+    var canvas = document.getElementById("sky");
+    var ctx = canvas.getContext("2d");
+
+    //set canvas dims to window height and width
+    var W = window.innerWidth;
+    var H = window.innerHeight;
+    canvas.width = W;
+    canvas.height = H;
+
+    //generate the snowflakes and apply attributes
+    var mf = 100; //max flakes
+    var flakes = [];
+
+    //loop through the empty flakes and apply attributes
+    for (var i = 0; i < mf; i++) {
+        flakes.push({
+            x: Math.random()*W,
+            y: Math.random()*H,
+            r: Math.random()*5+2, //min of 2px max of 7px
+            d: Math.random() + 1 //desteny
+        })
+    }
+    
+    //draw flakes onto canvas
+
+    
+
+}
